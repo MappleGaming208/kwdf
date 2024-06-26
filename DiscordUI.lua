61d6206d514e90515d4b054c97d59fdea3ff9568
MappleGaming208 <122428938+MappleGaming208@users.noreply.github.com>
2024-06-26 11:37:00 +0530

    Create DiscordUI.lua

---
+++ DiscordUI.lua
@@ -0,0 +1,8 @@
+
+_G.Settings = {
+	UI = {
+		Key = Enum.KeyCode.RightControl,
+	}
+}
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/SixZensED/Discord-Library/main/Library"))()
