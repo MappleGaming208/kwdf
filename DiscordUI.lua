e8874ba0cbbffb0086aee57a85fb151063fc2237
MappleGaming208 <122428938+MappleGaming208@users.noreply.github.com>
2024-06-26 11:43:17 +0530

    Update DiscordUI.lua

--- DiscordUI.lua
+++ DiscordUI.lua
@@ -1,8 +1,29 @@
-
+--Made By MappleGaming208--
+--Loadstring & Toggle UI--
+--RoyalUI By MappleGaming208--
 _G.Settings = {
 	UI = {
 		Key = Enum.KeyCode.RightControl,
 	}
 }
 
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/SixZensED/Discord-Library/main/Library"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/MappleGaming208/Discord-Library/main/Library"))()
+
+--Create UI Library Window--
+
+local main = Library:create({
+	Name = "RoyalUI"
+})
+
+--Create Tab--
+
+local tab = main:createtab({
+	Name = "Tab1"
+})
+
+--Create Section--
+
+local section = tab:createsection({
+Name = "Section"
+})
+
